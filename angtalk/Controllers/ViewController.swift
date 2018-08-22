a73577f145f6737ac9867f966d3f5f8a29479b69
pureye4u <pureye4u@gmail.com>
2018-08-22 15:30:04 +0900

    Initialize project

---
+++ angtalk/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  angtalk
+//
+//  Created by pureye4u on 22/08/2018.
+//  Copyright © 2018 slowslipper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
